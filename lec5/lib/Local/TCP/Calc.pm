b7a5bec4bdcf2d4462ed9112515208ae4bca7419
Ann <unbeman@gmail.com>
2016-10-29 14:53:54 +0300

    unpack header

--- lec5/lib/Local/TCP/Calc.pm
+++ lec5/lib/Local/TCP/Calc.pm
@@ -13,27 +13,30 @@ sub STATUS_DONE  {3}
 sub STATUS_ERROR {4}
 
 sub pack_header {
-	my $pkg = shift;
-	my $type = shift;
-	my $size = shift;
-	...
+	my $pkg 	= shift;
+	my $type 	= shift;
+	my $size 	= shift;
+	my $version = 1;
+
+	return pack('VVV', $version, $size, $type);
 }
 
 sub unpack_header {
-	my $pkg = shift;
-	my $header = shift;
-	...
+	my $pkg 	= shift;
+	my $header 	= shift;
+
+	return unpack('VVV', $header);
 }
 
 sub pack_message {
-	my $pkg = shift;
-	my $messages = shift;
+	my $pkg 	= shift;
+	my $msg 	= shift;
 	...
 }
 
 sub unpack_message {
-	my $pkg = shift;
-	my $message = shift;
+	my $pkg 	= shift;
+	my $msg 	= shift;
 	...
 }
 
